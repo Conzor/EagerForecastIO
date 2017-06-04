68d2e4a7fe193142f43589ac3a7e9bacac5b19a9
Teffen Ellis (Eric) <teffen@nirri.us>
2017-06-04 04:23:20 -0400

    Flesh out app refactor.

--- README.md
+++ README.md
@@ -1,7 +1,53 @@
-# Forecast
+# Example Webpack App
 
-*Having a rainy day?*
+This is our example app with the latest JavaScript and CSS features.
 
-Add Forecast so everyone knows their weather!
+Download this app and replace our code any time you want to build an Cloudflare app.
 
-When you install Forecast, people will be able to see the current weather, as well as the forecast for the next week
+**_Looking for a simpler setup? Check out our [vanilla example app](https://github.com/CloudflareApps/ExampleApp)!_**
+
+## Setup
+
+Fork and clone the repo or <a href="https://github.com/CloudflareApps/ExampleWebpackApp/releases/latest" >download the latest release</a>.
+
+Install the dependencies with `npm install`
+
+## Usage
+
+- `npm start` Watches for file changes and compile them.
+- `npm run build` Compiles your JavaScript and CSS once for release.
+- `npm run lint` Checks JavaScript formatting and attempts to fix any errors.
+
+## Details
+
+#### `source/app.js`
+
+This is where the magic happens. Your app starts here.
+
+#### `source/app.css`
+
+Styles for your app.
+
+#### `install.json`
+
+This is where all the <a href="https://www.cloudflare.com/apps/developer/docs/install-json">installer options</a> are added for the app.
+
+<a href="http://install.json.is/">Syntax can be tricky</a>, so be sure to double check it.
+
+#### `media/**`
+
+An directory for icons, tile images, and screenshots.
+
+[Download <code class="inline">media-templates.sketch</code>](https://github.com/CloudflareApps/MediaTemplates/raw/master/media-templates.sketch)
+
+### Troubleshooting
+
+- <a href="https://www.cloudflare.com/apps/developer/docs/getting-started">The Cloudflare developer documentation</a> for examples and API usage.
+
+<a href="https://www.cloudflare.com/apps/example-webpack-app/install?source=button">
+  <img
+    src="https://install.eager.io/install-button.png"
+    alt="Install Example Webpack App with Cloudflare"
+    border="0"
+    width="150">
+</a>
--- build/app.js
+++ build/app.js
@@ -1,105 +1,182 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
 "use strict";
 
-var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
+
+var _slicedToArray = (function () { function sliceIterator (arr, i) { var _arr = []; var _n = true; var _d = false; var _e; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break } } catch (err) { _d = true; _e = err } finally { try { if (!_n && _i['return']) _i['return']() } finally { if (_d) throw _e } } return _arr } return function (arr, i) { if (Array.isArray(arr)) { return arr } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i) } else { throw new TypeError('Invalid attempt to destructure non-iterable instance') } } }());
 
 (function () {
   // Check for IE9+
-  if (!window.addEventListener) return;
+  if (!window.addEventListener) return
 
-  var CALLBACK_NAME = "EagerForecastOnload";
-  var CONTAINER_HEIGHT = 245;
-  var PLACEHOLDER_ADDRESS = "1 Broadway Cambridge, MA 02142";
-  var RATE_LIMIT = 1500;
+  var CALLBACK_NAME = 'cloudflareAppForecastOnload'
+  var GOOGLE_KEY = 'AIzaSyDdz-2sV5sm8P0s-_F_o70w_M-sTfeuABQ'
+  var CONTAINER_HEIGHT = 245
+  var PLACEHOLDER_ADDRESS = 'San Francisco, CA 94107'
+  var RATE_LIMIT = 1500
 
-  var rateTimeout = undefined;
-  var element = undefined;
-  var script = undefined;
-  var options = INSTALL_OPTIONS;
-  var iframe = Object.assign(document.createElement("iframe"), {
-    type: "text/html",
-    frameBorder: "0",
-    height: CONTAINER_HEIGHT,
-    seamless: "seamless",
-    width: "100%"
-  });
+  var rateTimeout = void 0
+  var element = void 0
+  var script = void 0
+  var options = INSTALL_OPTIONS
+  var iframe = document.createElement('iframe')
 
-  function updateElement() {
-    var _options = options;
-    var colors = _options.colors;
-    var units = _options.units;
+  iframe.type = 'text/html'
+  iframe.frameBorder = '0'
+  iframe.height = CONTAINER_HEIGHT
+  iframe.seamless = 'seamless'
+  iframe.width = '100%'
 
-    var address = options.address.trim() || PLACEHOLDER_ADDRESS;
-    var _window$google$maps = window.google.maps;
-    var Geocoder = _window$google$maps.Geocoder;
-    var GeocoderStatus = _window$google$maps.GeocoderStatus;
+  function updateElement () {
+    var _options = options,
+      colors = _options.colors,
+      units = _options.units
 
-    var geocoder = new Geocoder();
+    var address = options.address.trim() || PLACEHOLDER_ADDRESS
+    var _window$google$maps = window.google.maps,
+      Geocoder = _window$google$maps.Geocoder,
+      GeocoderStatus = _window$google$maps.GeocoderStatus
 
-    element = Eager.createElement(options.element, element);
-    element.className = "eager-forecast";
-    element.style.height = CONTAINER_HEIGHT + "px";
+    var geocoder = new Geocoder()
+
+    element = INSTALL.createElement(options.element, element)
+    element.className = 'cloudflare-forecast'
+    element.style.height = CONTAINER_HEIGHT + 'px'
 
     geocoder.geocode({ address: address }, function (results, status) {
       if (status !== GeocoderStatus.OK) {
-        element.setAttribute("data-status", "error");
-        element.textContent = "Could not find the address, \"" + address + "\"";
-        return;
+        element.setAttribute('data-status', 'error')
+        element.textContent = 'Could not find the address, "' + address + '"'
+        return
       }
 
-      var _results$0 = results[0];
-      var formatted_address = _results$0.formatted_address;
-      var _results$0$geometry$location = _results$0.geometry.location;
-      var lat = _results$0$geometry$location.lat;
-      var lng = _results$0$geometry$location.lng;
+      var _results$ = results[0],
+        formatted_address = _results$.formatted_address,
+        _results$$geometry$lo = _results$.geometry.location,
+        lat = _results$$geometry$lo.lat,
+        lng = _results$$geometry$lo.lng
 
-      var _formatted_address$split = formatted_address.split(", ");
+      var _formatted_address$sp = formatted_address.split(', '),
+        _formatted_address$sp2 = _slicedToArray(_formatted_address$sp, 2),
+        city = _formatted_address$sp2[0],
+        _formatted_address$sp3 = _formatted_address$sp2[1],
+        stateAndZip = _formatted_address$sp3 === undefined ? '' : _formatted_address$sp3
 
-      var _formatted_address$split2 = _slicedToArray(_formatted_address$split, 2);
+      var _stateAndZip$split = stateAndZip.split(' '),
+        _stateAndZip$split2 = _slicedToArray(_stateAndZip$split, 1),
+        state = _stateAndZip$split2[0]
 
-      var city = _formatted_address$split2[0];
-      var _formatted_address$split2$1 = _formatted_address$split2[1];
-      var stateAndZip = _formatted_address$split2$1 === undefined ? "" : _formatted_address$split2$1;
+      var name = state ? city + ', ' + state : city
 
-      var _stateAndZip$split = stateAndZip.split(" ");
+      iframe.src = 'https://forecast.io/embed/#lat=' + lat() + '&lon=' + lng() + '&name=' + encodeURIComponent(name) + '&color=' + colors.tempColor + '&units=' + units
+      iframe.style.backgroundColor = colors.enableBackgroundColor ? colors.backgroundColor : ''
 
-      var _stateAndZip$split2 = _slicedToArray(_stateAndZip$split, 1);
-
-      var state = _stateAndZip$split2[0];
-
-      var name = state ? city + ", " + state : city;
-
-      iframe.src = "https://forecast.io/embed/#lat=" + lat() + "&lon=" + lng() + "&name=" + encodeURIComponent(name) + "&color=" + colors.tempColor + "&units=" + units;
-      iframe.style.backgroundColor = colors.enableBackgroundColor ? colors.backgroundColor : "";
-
-      element.appendChild(iframe);
-    });
+      element.appendChild(iframe)
+    })
   }
 
-  function updateScript() {
-    script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&callback=" + CALLBACK_NAME;
+  function updateScript () {
+    script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=' + GOOGLE_KEY + '&callback=' + CALLBACK_NAME
 
-    document.body.appendChild(script);
+    document.body.appendChild(script)
   }
 
-  window[CALLBACK_NAME] = updateElement;
+  window[CALLBACK_NAME] = updateElement
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateScript);
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', updateScript)
   } else {
-    updateScript();
+    updateScript()
   }
 
-  INSTALL_SCOPE = { // eslint-disable-line no-undef
-    setOptions: function setOptions(nextOptions) {
-      clearTimeout(rateTimeout);
-      options = nextOptions;
+  window.INSTALL_SCOPE = {
+    setOptions: function setOptions (nextOptions) {
+      clearTimeout(rateTimeout)
+      options = nextOptions
 
-      if (!window.google.maps) return updateScript();
+      if (!window.google.maps) {
+        return updateScript
 
       // Rapid requests are rejected by Google and must be limited.
-      rateTimeout = setTimeout(updateElement, RATE_LIMIT);
+      ()
+      }rateTimeout = setTimeout(updateElement, RATE_LIMIT)
     }
-  };
-})();
+  }
+})()
+
+
+/***/ })
+/******/ ]);
